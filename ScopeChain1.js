2edfb2666ac26d29522036a904ee359551172819
Mukesh <ganatram@gmail.com>
2026-03-19 11:16:32 +0530

    updatedES6Code

--- ScopeChain1.js
+++ ScopeChain1.js
@@ -1,9 +1,27 @@
 var color = 'blue';
 
+var x = 300;
+
 function changeColor() {
+  // SCA [changeColor(),window{}]
+
   var anotherColor = 'red';
 
+  var x = 200;
+
   function swapColors() {
+    //
+
+    function alert() {
+      console.log('alert on leave today');
+    }
+
+    alert('hi'); // [swapColors(),changeColor(),window]
+
+    var x = 100;
+
+    console.log(x); // SCA [swapColors(),changeColor(),window{}]
+
     var tempColor = anotherColor;
     anotherColor = color;
     color = tempColor;
